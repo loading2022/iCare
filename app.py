b1873237df9c3990c4244de3965290c61e6cb431
ShihPing0803 <119124535+loading2022@users.noreply.github.com>
2024-05-25 17:15:22 +0800

    Update app.py

--- app.py
+++ app.py
@@ -66,56 +66,6 @@ class ContinuousRecognizer:
 
 def create_did(text):
     result_url = "https://d-id-talks-prod.s3.us-west-2.amazonaws.com/google-oauth2%7C105929470202191278895/tlk_cylcop6sef76SriF4Or7h/1716552186447.mp4?AWSAccessKeyId=AKIA5CUMPJBIK65W6FGA&Expires=1716638589&Signature=%2FAMUEa63lViaedVmoQGk%2FgqVDX4%3D"
-    """
-    url = "https://api.d-id.com/talks"
-
-    payload = {
-    "script": {
-        "type": "text",
-        "subtitles": "false",
-        "provider": {
-            "type": "microsoft",
-            "voice_id": "zh-CN-XiaoxiaoNeural"
-        },
-        "ssml": "false",
-        "input": text
-    },
-    "config": {
-        "fluent": "false",
-        "pad_audio": "0.0"
-    },
-    "source_url": "https://create-images-results.d-id.com/api_docs/assets/noelle.jpeg"
-    }
-    headers = {
-        "accept": "application/json",
-        "content-type": "application/json",
-        "authorization": "Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6Ik53ek53TmV1R3ptcFZTQjNVZ0J4ZyJ9.eyJodHRwczovL2QtaWQuY29tL2ZlYXR1cmVzIjoiIiwiaHR0cHM6Ly9kLWlkLmNvbS9zdHJpcGVfcHJvZHVjdF9pZCI6InByb2RfTXRkY2RrM29nR3hkNlkiLCJodHRwczovL2QtaWQuY29tL3N0cmlwZV9jdXN0b21lcl9pZCI6ImN1c19ROXF5a0E4UThTWnR4SyIsImh0dHBzOi8vZC1pZC5jb20vc3RyaXBlX3Byb2R1Y3RfbmFtZSI6ImxpdGUtbW9udGgtNjQiLCJodHRwczovL2QtaWQuY29tL3N0cmlwZV9zdWJzY3JpcHRpb25faWQiOiJzdWJfMVBKWEdESnhFS1oyekF5blVMRldtVjF0IiwiaHR0cHM6Ly9kLWlkLmNvbS9zdHJpcGVfYmlsbGluZ19pbnRlcnZhbCI6Im1vbnRoIiwiaHR0cHM6Ly9kLWlkLmNvbS9zdHJpcGVfcGxhbl9ncm91cCI6ImRlaWQtbGl0ZSIsImh0dHBzOi8vZC1pZC5jb20vc3RyaXBlX3ByaWNlX2lkIjoicHJpY2VfMU5TYmR6SnhFS1oyekF5bmNYN1dRVWoxIiwiaHR0cHM6Ly9kLWlkLmNvbS9zdHJpcGVfcHJpY2VfY3JlZGl0cyI6IjY0IiwiaHR0cHM6Ly9kLWlkLmNvbS9jaGF0X3N0cmlwZV9zdWJzY3JpcHRpb25faWQiOiIiLCJodHRwczovL2QtaWQuY29tL2NoYXRfc3RyaXBlX3ByaWNlX2NyZWRpdHMiOiIiLCJodHRwczovL2QtaWQuY29tL2NoYXRfc3RyaXBlX3ByaWNlX2lkIjoiIiwiaHR0cHM6Ly9kLWlkLmNvbS9wcm92aWRlciI6Imdvb2dsZS1vYXV0aDIiLCJodHRwczovL2QtaWQuY29tL2lzX25ldyI6ZmFsc2UsImh0dHBzOi8vZC1pZC5jb20vYXBpX2tleV9tb2RpZmllZF9hdCI6IjIwMjQtMDUtMjNUMDg6NTI6NDUuMTc0WiIsImh0dHBzOi8vZC1pZC5jb20vb3JnX2lkIjoiIiwiaHR0cHM6Ly9kLWlkLmNvbS9hcHBzX3Zpc2l0ZWQiOlsiU3R1ZGlvIl0sImh0dHBzOi8vZC1pZC5jb20vY3hfbG9naWNfaWQiOiIiLCJodHRwczovL2QtaWQuY29tL2NyZWF0aW9uX3RpbWVzdGFtcCI6IjIwMjMtMTEtMjNUMDg6MTM6MjEuMTQ4WiIsImh0dHBzOi8vZC1pZC5jb20vYXBpX2dhdGV3YXlfa2V5X2lkIjoiYmo4eHpxODdkMiIsImh0dHBzOi8vZC1pZC5jb20vdXNhZ2VfaWRlbnRpZmllcl9rZXkiOiJ1c2dfQUU3QXhGWERucTdzOWtFYnJHbjR4IiwiaHR0cHM6Ly9kLWlkLmNvbS9oYXNoX2tleSI6InBlZTlNOUd0OE82eFAtRi1teXNjdSIsImh0dHBzOi8vZC1pZC5jb20vcHJpbWFyeSI6dHJ1ZSwiaHR0cHM6Ly9kLWlkLmNvbS9lbWFpbCI6IjExMXl6dWltYmlsYWJAZ21haWwuY29tIiwiaHR0cHM6Ly9kLWlkLmNvbS9wYXltZW50X3Byb3ZpZGVyIjoic3RyaXBlIiwiaXNzIjoiaHR0cHM6Ly9hdXRoLmQtaWQuY29tLyIsInN1YiI6Imdvb2dsZS1vYXV0aDJ8MTA1OTI5NDcwMjAyMTkxMjc4ODk1IiwiYXVkIjpbImh0dHBzOi8vZC1pZC51cy5hdXRoMC5jb20vYXBpL3YyLyIsImh0dHBzOi8vZC1pZC51cy5hdXRoMC5jb20vdXNlcmluZm8iXSwiaWF0IjoxNzE2NTQ1MjE1LCJleHAiOjE3MTY2MzE2MTUsInNjb3BlIjoib3BlbmlkIHByb2ZpbGUgZW1haWwgcmVhZDpjdXJyZW50X3VzZXIgdXBkYXRlOmN1cnJlbnRfdXNlcl9tZXRhZGF0YSBvZmZsaW5lX2FjY2VzcyIsImF6cCI6Ikd6ck5JMU9yZTlGTTNFZURSZjNtM3ozVFN3MEpsUllxIn0.OcGfh3TJ0uInXoEUmEp6ddTuYKrsEQw6zrraVxaNWnRy95LVG_339scDFhOQJbauMPiYDgW-BhYyq-dVKykKAwPmeWOnQbPHtaAx3BToAVT8r3Kxv52Nq7Hxr2L3VI6RuiQC3POPSiWUyr5B1NpS9nHLfAiThooyMZ3m1_oGPgwQfOUaotxJS95z39DEHDBNYdm35dNDDAQ275KxUQ_-WcLW6xAiJPiN79c2Uiuv11AR6S4M__juRbvtiTbAkrqr2IZ5W2ajdwj5FdFIvGYtUQv6_tW-jK4XXlxsEfuF2TSd1HnNJ_vOU4-3H8oONU0oLxxncgEJe10ttFnEjP-hFw"
-    }
-
-    response = requests.post(url, json=payload, headers=headers)
-    response_json = json.loads(response.text)
-    print(response_json)
-
-    talk_id = response_json['id']
-    url = "https://api.d-id.com/talks/"+talk_id
-
-    headers = {
-        "accept": "application/json",
-        "authorization": "Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6Ik53ek53TmV1R3ptcFZTQjNVZ0J4ZyJ9.eyJodHRwczovL2QtaWQuY29tL2ZlYXR1cmVzIjoiIiwiaHR0cHM6Ly9kLWlkLmNvbS9zdHJpcGVfcHJvZHVjdF9pZCI6InByb2RfTXRkY2RrM29nR3hkNlkiLCJodHRwczovL2QtaWQuY29tL3N0cmlwZV9jdXN0b21lcl9pZCI6ImN1c19ROXF5a0E4UThTWnR4SyIsImh0dHBzOi8vZC1pZC5jb20vc3RyaXBlX3Byb2R1Y3RfbmFtZSI6ImxpdGUtbW9udGgtNjQiLCJodHRwczovL2QtaWQuY29tL3N0cmlwZV9zdWJzY3JpcHRpb25faWQiOiJzdWJfMVBKWEdESnhFS1oyekF5blVMRldtVjF0IiwiaHR0cHM6Ly9kLWlkLmNvbS9zdHJpcGVfYmlsbGluZ19pbnRlcnZhbCI6Im1vbnRoIiwiaHR0cHM6Ly9kLWlkLmNvbS9zdHJpcGVfcGxhbl9ncm91cCI6ImRlaWQtbGl0ZSIsImh0dHBzOi8vZC1pZC5jb20vc3RyaXBlX3ByaWNlX2lkIjoicHJpY2VfMU5TYmR6SnhFS1oyekF5bmNYN1dRVWoxIiwiaHR0cHM6Ly9kLWlkLmNvbS9zdHJpcGVfcHJpY2VfY3JlZGl0cyI6IjY0IiwiaHR0cHM6Ly9kLWlkLmNvbS9jaGF0X3N0cmlwZV9zdWJzY3JpcHRpb25faWQiOiIiLCJodHRwczovL2QtaWQuY29tL2NoYXRfc3RyaXBlX3ByaWNlX2NyZWRpdHMiOiIiLCJodHRwczovL2QtaWQuY29tL2NoYXRfc3RyaXBlX3ByaWNlX2lkIjoiIiwiaHR0cHM6Ly9kLWlkLmNvbS9wcm92aWRlciI6Imdvb2dsZS1vYXV0aDIiLCJodHRwczovL2QtaWQuY29tL2lzX25ldyI6ZmFsc2UsImh0dHBzOi8vZC1pZC5jb20vYXBpX2tleV9tb2RpZmllZF9hdCI6IjIwMjQtMDUtMjNUMDg6NTI6NDUuMTc0WiIsImh0dHBzOi8vZC1pZC5jb20vb3JnX2lkIjoiIiwiaHR0cHM6Ly9kLWlkLmNvbS9hcHBzX3Zpc2l0ZWQiOlsiU3R1ZGlvIl0sImh0dHBzOi8vZC1pZC5jb20vY3hfbG9naWNfaWQiOiIiLCJodHRwczovL2QtaWQuY29tL2NyZWF0aW9uX3RpbWVzdGFtcCI6IjIwMjMtMTEtMjNUMDg6MTM6MjEuMTQ4WiIsImh0dHBzOi8vZC1pZC5jb20vYXBpX2dhdGV3YXlfa2V5X2lkIjoiYmo4eHpxODdkMiIsImh0dHBzOi8vZC1pZC5jb20vdXNhZ2VfaWRlbnRpZmllcl9rZXkiOiJ1c2dfQUU3QXhGWERucTdzOWtFYnJHbjR4IiwiaHR0cHM6Ly9kLWlkLmNvbS9oYXNoX2tleSI6InBlZTlNOUd0OE82eFAtRi1teXNjdSIsImh0dHBzOi8vZC1pZC5jb20vcHJpbWFyeSI6dHJ1ZSwiaHR0cHM6Ly9kLWlkLmNvbS9lbWFpbCI6IjExMXl6dWltYmlsYWJAZ21haWwuY29tIiwiaHR0cHM6Ly9kLWlkLmNvbS9wYXltZW50X3Byb3ZpZGVyIjoic3RyaXBlIiwiaXNzIjoiaHR0cHM6Ly9hdXRoLmQtaWQuY29tLyIsInN1YiI6Imdvb2dsZS1vYXV0aDJ8MTA1OTI5NDcwMjAyMTkxMjc4ODk1IiwiYXVkIjpbImh0dHBzOi8vZC1pZC51cy5hdXRoMC5jb20vYXBpL3YyLyIsImh0dHBzOi8vZC1pZC51cy5hdXRoMC5jb20vdXNlcmluZm8iXSwiaWF0IjoxNzE2NTQ1MjE1LCJleHAiOjE3MTY2MzE2MTUsInNjb3BlIjoib3BlbmlkIHByb2ZpbGUgZW1haWwgcmVhZDpjdXJyZW50X3VzZXIgdXBkYXRlOmN1cnJlbnRfdXNlcl9tZXRhZGF0YSBvZmZsaW5lX2FjY2VzcyIsImF6cCI6Ikd6ck5JMU9yZTlGTTNFZURSZjNtM3ozVFN3MEpsUllxIn0.OcGfh3TJ0uInXoEUmEp6ddTuYKrsEQw6zrraVxaNWnRy95LVG_339scDFhOQJbauMPiYDgW-BhYyq-dVKykKAwPmeWOnQbPHtaAx3BToAVT8r3Kxv52Nq7Hxr2L3VI6RuiQC3POPSiWUyr5B1NpS9nHLfAiThooyMZ3m1_oGPgwQfOUaotxJS95z39DEHDBNYdm35dNDDAQ275KxUQ_-WcLW6xAiJPiN79c2Uiuv11AR6S4M__juRbvtiTbAkrqr2IZ5W2ajdwj5FdFIvGYtUQv6_tW-jK4XXlxsEfuF2TSd1HnNJ_vOU4-3H8oONU0oLxxncgEJe10ttFnEjP-hFw"
-    }
-    while True:
-        response = requests.get(url, headers=headers)
-        response_json = json.loads(response.text)
-        status = response_json['status']
-        if status == 'done':
-            break
-    
-    result_url = response_json['result_url']
-    response = requests.get(result_url)
-    filename = "C:\\Users\\user\\Desktop\\lab\\icare\\record.mp4"
-    with open(filename, 'wb') as f:
-        f.write(response.content)
-    """
     return result_url
 
 @app.route('/')
